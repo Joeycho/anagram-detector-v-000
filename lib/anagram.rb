b3e20d2e3d6eaf6073bcbc1f101507a6ba4c2153
Joey Cho <cjy12182000@naver.com>
2018-06-05 13:31:01 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(otherword)
+    candidate = otherword.split(/\s/)
+    candidate.map do{|x|
+      if x.length != self.word.length
+        candidate.delete(x)
+      elsif x.split("").sort == self.word.split("").sort
+        yield x
+      end
+    }
+  end
+end
